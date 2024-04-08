32da0cc7b9e8b2530a1a34a1b54513cb68d60138
amit <pmitenrique@gmail.com>
2024-04-08 16:11:27 +0100

    terraform doest track tree folder structure

--- webSubnet.tf
+++ webSubnet.tf
@@ -5,17 +5,17 @@ resource "aws_subnet" "web-subnet-1"{
     availability_zone= "us-east-1a"
     map_public_ip_on_launch = true
     tags={
-        Name= pub-1
+        Name= "pub-1"
     }
 }
 
 # Second public subnet
-resource "aws_subnet" "web-subnet-1"{
+resource "aws_subnet" "web-subnet-2"{
     vpc_id=aws_vpc.vpc.id 
     cidr_block="10.0.2.0/24"
     availability_zone= "us-east-1b"
     map_public_ip_on_launch = true
     tags={
-        Name= pub-2
+        Name= "pub-2"
     }
 }
